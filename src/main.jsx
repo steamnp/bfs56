3b7c142827442af4e765ca7bb3a2e20f72ec6c46
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-07-24 00:22:42 +0545

    css box model

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,5 @@ import './main.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className="box">Hello</div>
-    <span className="inline">Hello</span>
-    <span>I am another inline</span>
-    <div className="section">I am section</div>
-    <button className="btn">Click Me</button>
   </React.StrictMode>
 )

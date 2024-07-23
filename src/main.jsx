32978d567c5abb87fa0f982beeb1ea34365a7ba6
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-07-23 23:56:11 +0545

    html and css

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './main.css'
 
-// JSX (is equivalent form of html)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <form action="/action_page.php">
-      <label htmlFor="fname">First name:</label>
-      <br />
-      <input type="text" id="fname" name="fname" value="John" />
-      <br />
-      <label htmlFor="lname">Last name:</label>
-      <br />
-      <input type="text" id="lname" name="lname" value="Doe" />
-      <br />
-      <br />
-      <input type="submit" value="Submit" />
-    </form>
+    <main className="container">
+      <section className="sectionone">
+        <div className="boxone">
+          <h1 className="headingone">Heading 1</h1>
+          <p className="paragraphone">Paragraph 1</p>
+        </div>
+      </section>
+      <section className="sectiontwo">
+        <div className="boxtwo">
+          <h1 className="headingtwo">Heading 2</h1>
+          <p className="paragraphtwo">Paragraph 2</p>
+        </div>
+      </section>
+    </main>
   </React.StrictMode>
 )
-// class -> className
-// for -> htmlFor

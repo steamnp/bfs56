d0b9d12220b8a79adf7dd3f03147c198f8cbd801
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-07-23 07:28:50 -0700

    intialize react app

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
 
-// camelCase -> gorakhRajJoshi -> while creating javascript variable
-// PascalCase -> GorakhRajJoshi -> While creating component
-// kebab-case -> gorakh-raj-joshi -> While creating files or folders
-
-// A component name should be written in PascalCase
-// This is App component
-function App() {
-  return <div className="box">Hello</div>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <header>
+      <div className="heading">
+        <h1>Nima sherpa</h1>
+        <p>Web Developer & Designer</p>
+      </div>
+      <section className="about">
+        <h2>About Me</h2>
+        <p>
+          I am a passionate web developer with a love for creating beautiful and
+          functional websites.
+        </p>
+      </section>
+      <section className="projects">
+        <h2>Projects</h2>
+        <div className="project1">
+          <h3>Project1</h3>
+          <p>A web application for managing tasks.</p>
+        </div>
+        <div className="project2">
+          <h3>Project 2</h3>
+          <p>A portfolio website for showcasing my work.</p>
+        </div>
+        <div className="project3">
+          <h3>Project 2</h3>
+          <p>An e-commerce site for selling products online.</p>
+        </div>
+      </section>
+      <section className="contact">
+        <h2>Contact</h2>
+        <p>Email : nimanurusherpa2022@gmail.com</p>
+      </section>
+    </header>
   </React.StrictMode>
-)
+);

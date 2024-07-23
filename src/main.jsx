96605452750874ed8a623245b87eb0382ea0d5d9
lhakpa75 <lhakpa@tumen.nyc>
2024-07-23 16:00:34 -0400

    Task1

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <header>I am header</header>
-    <main>
-      <section>I am title section of main</section>
-      <section>I am body section of main</section>
-    </main>
-    <div className="box"></div>
-    <footer>I am footer</footer>
+    <header className="header"> My Portfolio</header>
+    <section className="about">
+      <h2>About Me</h2>
+      <p>I am learning to code at Takeo.</p>
+    </section>
+    <section className="project">
+      <h2>Projects</h2>
+    </section>
+    <div className="project">
+      <h3>Week One</h3>
+      <p>Vs Code & github</p>
+    </div>
+    <div className="project">
+      <h3>Week Two</h3>
+      <p>Html & Css</p>
+    </div>
+    <div className="project">
+      <h3>Week Three</h3>
+      <p>GitHub Copilot</p>
+    </div>
+    <footer className="contact">
+      <h2>Contact</h2>
+      <p>lhakpa@tumen.nyc</p>
+    </footer>
   </React.StrictMode>
-)
+);

8a909d6f3197f821c71252aaf52af82d65036037
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-07-24 00:09:04 +0545

    inline and block element

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './main.css'
 
+// block level element
+// inline element
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <main className="container">
-      <section className="sectionone">
-        <div className="boxone">
-          <h1 className="headingone">Heading 1</h1>
-          <p className="paragraphone">Paragraph 1</p>
-        </div>
-      </section>
-      <section className="sectiontwo">
-        <div className="boxtwo">
-          <h1 className="headingtwo">Heading 2</h1>
-          <p className="paragraphtwo">Paragraph 2</p>
-        </div>
-      </section>
-    </main>
+    <div className="box">Hello</div>
+    <span className="inline">Hello</span>
+    <span>I am another inline</span>
+    <div className="section">I am section</div>
+    <button className="btn">Click Me</button>
   </React.StrictMode>
 )

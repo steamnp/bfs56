d3c73d1f058ccee1232e797884ef9b3f72f362db
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-07-23 20:42:29 +0545

    clean initial code

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-//font size:n normal x font size
-/* CSS Box Model */
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <header className="header">
-      <h1 className="heading1">Lhakpa Sherpa</h1>
-      <p className="job-title">Web Developer & Designer</p>
-    </header>
-    <section className="about-me">
-      <div className="">
-        <h3 className="heading3">About Me</h3>
-        <p className="discription">
-          I am a passionate web developer with a love for beautiful and
-          functional website.
-        </p>
-      </div>
-    </section>
-    <section className="project-section">
-      <div className="project">
-        <h3 className="heading3">Projects</h3>
-        <div className="project1">
-          <h4>Project1</h4>
-          <p>A web Application for managing tasks.</p>
-        </div>
-        <div className="project1">
-          <h4>Project1</h4>
-          <p>A portfolio website for showcasing my work.</p>
-        </div>
-        <div className="project1">
-          <h4>Project1</h4>
-          <p>An e-commerce site for selling products online.</p>
-        </div>
-      </div>
-    </section>
-    <section className="contact">
-      <div className="contact-section">
-        <h3 className="contact">Contact</h3>
-        <p className="email">Email: sandeebadhikari@gmail.com</p>
-      </div>
-    </section>
+    <h1>Hello. I am Gorakh.</h1>
   </React.StrictMode>
-);
+)

ec82130b810b61784395e692e4b92def3afeede4
Amit Gahatraj <amitgahatraj4@gmail.com>
2024-07-23 15:50:32 -0400

    HTML practice

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,47 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './main.css'
 
-// camelCase -> gorakhRajJoshi -> while creating javascript variable
-// PascalCase -> GorakhRajJoshi -> While creating component
-// kebab-case -> gorakh-raj-joshi -> While creating files or folders
-
-// A component name should be written in PascalCase
-// This is App component
-function App() {
-  return <div className="box">Hello</div>
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <header>This is header</header>
-    <main>
-      <section>This is title section.</section>
-    </main>
-  </React.StrictMode>
-    <App />
+    {/* <section className="header">
+      <h1>Amit Gahatraj</h1>
+      <br />
+      <p>Student</p>
+    </section> */}
+
+    <header>
+      <h1>Amit Gahatraj</h1>
+      <br />
+      <p>Student</p>
+    </header>
+
+    <section className="aboutme">
+      <h2>About Me</h2>
+      <br />
+      <p>A student learning HTML and CSS.</p>
+    </section>
+
+    <section className="projects">
+      <h2>Projects</h2>
+      <div className="p1">
+        <h3>Project One</h3>
+        <p>This is project one.</p>
+      </div>
+
+      <div className="p2">
+        <h3>Project Two</h3>
+        <p>This is project two.</p>
+      </div>
+
+      <div className="p3">
+        <h3>Project Three</h3>
+        <p>This is project three.</p>
+      </div>
+    </section>
+
+    <section className="contacts">
+      <h2>Contact</h2>
+      <p>sample@gmail.com</p>
+    </section>
   </React.StrictMode>
 )

7cfa012cfec042603ed4c167975f9fdd90c079c5
adhsandb05 <adhikarisandeeb@gmail.com>
2024-07-23 14:43:58 -0400

    CSS BOX MODEL

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
 
-//block level element : div, section, main, footer, header to make it block element
-// display: inline; in css
+//font size:n normal x font size
+/* CSS Box Model */
 
-//inline level element : span, button, to make it inline element
-//display: block; in css
-
-//to know what to use block level element or inline level
-//look for Semantic of html
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="box">Hello</div>
-    <span className="inline">Hello</span>
-    <span className="inline2">I am another inline</span>
-    <section className="section">I am section</section>
-    <button className="btn">Click Me</button>
   </React.StrictMode>
 );

c0efa71a30042b0676baef8b87b267cc53144eda
adhsandb05 <adhikarisandeeb@gmail.com>
2024-07-23 11:52:36 -0400

    modified <React.stricMode>

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,9 @@ import ReactDOM from "react-dom/client";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <h1>Hello, I am Sandeeb Adhikari.</h1>
+    <p>
+      Student of the Takeo BootCamp taught by Mr Gorakh Raj Josh. If you wanna
+      enroll on Takeo Bootcamp. <a href="https://www.takeo.ai/">Click Here!</a>
+    </p>
   </React.StrictMode>
 );

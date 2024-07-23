431e6fbfe23dc77d80c10b3d32aa1982fc3ffdd3
adhsandb05 <adhikarisandeeb@gmail.com>
2024-07-23 12:09:25 -0400

    .updated css

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hello, I am Sandeeb Adhikari.</h1>
-    <p>
-      Student of the Takeo BootCamp taught by Mr Gorakh Raj Josh. If you wanna
-      enroll on Takeo Bootcamp. <a href="https://www.takeo.ai/">Click Here!</a>
-    </p>
+    <header>I am header</header>
+    <main>
+      <section>I am title section of main</section>
+      <section>I am body section of main</section>
+    </main>
+    <div className="box"></div>
+    <footer> I am footer </footer>
   </React.StrictMode>
 );

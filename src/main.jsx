9e934407dc6cddc381499196961217220bb728e4
Shiv Shankar Yadav <shivjanakpur@gmail.com>
2024-07-23 08:13:18 -0700

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+<<<<<<< HEAD
 import './main.css'
 
 // camelCase -> gorakhRajJoshi -> while creating javascript variable
@@ -16,4 +17,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+=======
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Hi, I am Shiv</h1>
+  </React.StrictMode>,
+>>>>>>> ed95ec8 (Updated)
 )

79dba31136a8daa0313c68c91ed3f21afeb28b84
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-07-23 21:54:17 +0545

    implement html and css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <header>I am header</header>
+    <main>
+      <section>I am title section of main</section>
+      <section>I am body section of main</section>
+    </main>
+    <div className="box"></div>
+    <footer>I am footer</footer>
   </React.StrictMode>
 )

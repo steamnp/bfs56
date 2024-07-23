a72d8320f3c3ed9cf03c4104c74e1129032d4a30
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-07-23 23:39:41 +0545

    html vs jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './main.css'
 
+// JSX (is equivalent form of html)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <header>I am header</header>
-    <main>
-      <section>I am title section of main</section>
-      <section>I am body section of main</section>
-    </main>
-    <div className="box"></div>
-    <footer>I am footer</footer>
+    <form action="/action_page.php">
+      <label htmlFor="fname">First name:</label>
+      <br />
+      <input type="text" id="fname" name="fname" value="John" />
+      <br />
+      <label htmlFor="lname">Last name:</label>
+      <br />
+      <input type="text" id="lname" name="lname" value="Doe" />
+      <br />
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
   </React.StrictMode>
 )
+// class -> className
+// for -> htmlFor

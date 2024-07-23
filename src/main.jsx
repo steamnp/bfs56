bedef1e498a31dfe6c5e5e7101ea181ade6eafad
Ronisha Shigdel <ronishashigdel@gmail.com>
2024-07-23 12:37:15 -0700

    portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Sandeeb from "./sandeeb";
+import "./main.css";
+
+// camelCase -> gorakhRajJoshi -> while creating javascript variable
+// PascalCase -> GorakhRajJoshi -> While creating component
+// kebab-case -> gorakh-raj-joshi -> While creating files or folders
 
 // A component name should be written in PascalCase
 // This is App component
+function App() {
+  return <div className="box">Hello</div>;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

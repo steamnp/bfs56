3863de46030eec2f439bcbfcad49ba87d6100a46
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-07-24 21:47:45 +0545

    create react component

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './main.css'
 
-// block level element
-// inline element
+// camelCase -> gorakhRajJoshi -> while creating javascript variable
+// PascalCase -> GorakhRajJoshi -> While creating component
+// kebab-case -> gorakh-raj-joshi -> While creating files or folders
+
+// A component name should be written in PascalCase
+// This is App component
+function App() {
+  return <div className="box">Hello</div>
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className="box">Hello</div>
+    <App />
   </React.StrictMode>
 )

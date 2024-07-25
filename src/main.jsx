12ebd06e7d7d62c9997b05dcc16d7fe62220fae7
lhakpa75 <lhakpa@tumen.nyc>
2024-07-25 11:44:17 -0400

    Day 4

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Lhakpa from "./lhakpa";
 
-// camelCase -> gorakhRajJoshi -> while creating javascript variable
-// PascalCase -> GorakhRajJoshi -> While creating component
-// kebab-case -> gorakh-raj-joshi -> While creating files or folders
-
-// A component name should be written in PascalCase
-// This is App component
-function Lhakpa() {
-  return <div className="box">Hello</div>;
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Sandeeb />
+    <Lhakpa />
   </React.StrictMode>
 );
 );

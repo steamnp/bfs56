07ecf5cf1a024341efbfd31c11d6a3a20de4ff30
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-07-25 11:21:22 -0400

    created a Manish component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Sandeeb from "./sandeeb";
-
-// A component name should be written in PascalCase
-// This is App component
+import Manish from "./manish";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Sandeeb />
+    <Manish />
   </React.StrictMode>
+);
 );
---
+++ src/manish.jsx
@@ -0,0 +1,5 @@
+function Manish() {
+  return <h1>Hi I am Manish.</h1>;
+}
+
+export default Manish;

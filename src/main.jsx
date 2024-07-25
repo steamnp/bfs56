2bd97987de7b676aa5e4a1c3f9c6a45744034bf6
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-07-25 11:42:27 -0400

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import Manish from "./manish";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Manish />
   </React.StrictMode>
-);
 );

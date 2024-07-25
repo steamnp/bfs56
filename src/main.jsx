ff47b38ddb5a1c2bb7e3d03fbf6a3230cc9aa3e7
venkatsaikrishnareddy <saikrishnaray0@gmail.com>
2024-07-25 12:02:01 -0400

    NEWCODE1

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./main.css";
-
-// block level element
-// inline element
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./main.css";
-
-// camelCase -> gorakhRajJoshi -> while creating javascript variable
-// PascalCase -> GorakhRajJoshi -> While creating component
-// kebab-case -> gorakh-raj-joshi -> While creating files or folders
-
-// A component name should be written in PascalCase
-// This is App component
-function App() {
-  return <div className="box">Hello</div>;
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-R;
+import Krish from "./krish";
 ReactDOM.createRoot(document.getElementById("root")).render(
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -28,5 +9,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Krish />
   </React.StrictMode>
-);
 );

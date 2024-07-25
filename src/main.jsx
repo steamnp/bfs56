263482610a93f85a4286a4d2135f3425b68acca8
adhsandb05 <adhikarisandeeb@gmail.com>
2024-07-25 11:18:17 -0400

    components

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './main.css'
-
-// camelCase -> gorakhRajJoshi -> while creating javascript variable
-// PascalCase -> GorakhRajJoshi -> While creating component
-// kebab-case -> gorakh-raj-joshi -> While creating files or folders
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Sandeeb from "./sandeeb";
 
 // A component name should be written in PascalCase
 // This is App component
-function App() {
-  return <div className="box">Hello</div>
-}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Sandeeb />
   </React.StrictMode>
-)
+);

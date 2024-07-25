5736777986d1aa99e573e627543eb14d34b48183
Ronisha Shigdel <ronishashigdel@gmail.com>
2024-07-25 08:27:18 -0700

    add component

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
-
-// camelCase -> gorakhRajJoshi -> while creating javascript variable
-// PascalCase -> GorakhRajJoshi -> While creating component
-// kebab-case -> gorakh-raj-joshi -> While creating files or folders
-
-// A component name should be written in PascalCase
-// This is App component
-function App() {
-  return <div className="box">Hello</div>;
-}
+import "./styles.css";
+import Ronisha from "./ronisha";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Sandeeb />
+    <Ronisha />
   </React.StrictMode>
+);
 );
---
+++ src/ronisha.jsx
@@ -0,0 +1,5 @@
+function Ronisha() {
+  return <h1>Hi, I'm Ronisha.</h1>;
+}
+
+export default Ronisha;

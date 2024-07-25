f5e26b2ca70fc69aababc0a6f723e6ec23daeb11
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-07-25 08:37:42 -0700

    fix conflict

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import Nima from "./nima";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Nima />
   </React.StrictMode>
-);
 );
--- src/sandeeb.jsx
+++
@@ -1,5 +0,0 @@
-function Sandeeb() {
-  return <h1>Hi, This is Nima</h1>;
-}
-
-export default Sandeeb;

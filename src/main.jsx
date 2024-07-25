4c2181472bf46bad1868603479ed2b842a3705bd
Shiv Shankar Yadav <shivjanakpur@gmail.com>
2024-07-25 08:34:32 -0700

    Shiv's Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import './main.css'
-
-// camelCase -> gorakhRajJoshi -> while creating javascript variable
-// PascalCase -> GorakhRajJoshi -> While creating component
-// kebab-case -> gorakh-raj-joshi -> While creating files or folders
-
-// A component name should be written in PascalCase
-// This is App component
-function App() {
-  return <div className="box">Hello</div>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Shiv from "./shiv";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <h1>Hi, I am Shiv</h1>
-  </React.StrictMode>,
->>>>>>> ed95ec8 (Updated)
-)
+  </React.StrictMode>
+);
---
+++ src/shiv.jsx
@@ -0,0 +1,5 @@
+function Shiv() {
+  return <h2> Hi!, I am Shiv</h2>;
+}
+
+export default Shiv;

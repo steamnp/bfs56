6591187e9d7e60dbe53112cbe253ac221500e95f
adhsandb05 <adhikarisandeeb@gmail.com>
2024-07-25 11:18:17 -0400

    components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./main.css";
-import "./styles.css";
 import Ronisha from "./ronisha";
 
 ReactDOM.createRoot(document.getElementById("root")).render(

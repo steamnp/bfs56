961c7beddbb2246b5eaf3d1899c994a3664b3843
Amit Gahatraj <amitgahatraj4@gmail.com>
2024-07-25 11:27:34 -0400

    Use Component in React

---
+++ src/amit.jsx
@@ -0,0 +1,7 @@
+// this is app.jsx named as amit.jsx
+
+function Amit() {
+  return <h1>Hi! This is made using React Component</h1>;
+}
+
+export default Amit;
--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./main.css";
+import Amit from "./amit";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <section className="header">
-      <h1>Amit Gahatraj</h1>
-      <br />
-      <p>Student</p>
-    </section> */}
-
-    <header>
-      <h1>Amit Gahatraj</h1>
-      <p>Student</p>
-    </header>
-
-    <section className="aboutme">
-      <h2>About Me</h2>
-      <br />
-      <p>A student learning HTML and CSS.</p>
-    </section>
-
-    <section className="projects">
-      <h2>Projects</h2>
-      <div className="p1">
-        <h3>Project One</h3>
-        <p>This is project one.</p>
-      </div>
-
-      <div className="p2">
-        <h3>Project Two</h3>
-        <p>This is project two.</p>
-      </div>
-
-      <div className="p3">
-        <h3>Project Three</h3>
-        <p>This is project three.</p>
-      </div>
-    </section>
-
-    <footer className="contacts">
-      <h2>Contact</h2>
-      <p>sample@gmail.com</p>
-    </footer>
+    <Amit />
   </React.StrictMode>
-)
+);

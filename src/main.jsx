9ab7f2f34b2cd7bb2a23afc9b7035183f741759a
Practice <gurungriwaaz7@gmail.com>
2024-07-28 14:14:20 -0400

    initialize react app

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Sandeeb from "./sandeeb";
-
-// A component name should be written in PascalCase
-// This is App component
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Sandeeb />
+    <App />
   </React.StrictMode>
 );

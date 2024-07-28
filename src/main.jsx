510008428e70942391a5c6721a1ed1f653fb174f
Practice <gurungriwaaz7@gmail.com>
2024-07-28 15:24:17 -0400

    basin learning

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,8 @@ import ReactDOM from "react-dom/client";
 ReactDOM.createRoot(document.getElementById("root")).render(
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-    <h1>Hello, I amDurga Gurung.</h1>
+    <h1>Hello, I am Durga Gurung.</h1>
+    <p> My plane</p>
+    <img src="Images/istockphoto-155439315-1024x1024.jpg" />
   </React.StrictMode>
 );

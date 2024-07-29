9c47f4643496243aa0f34174156aedd49b554ce6
Ronisha Shigdel <ronishashigdel@gmail.com>
2024-07-29 07:27:16 -0700

    props and validation

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Ronisha from "./ronisha";
 
+//props are age, favNumber
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Ronisha />
+    <Ronisha age={25} favNumber={13} />
   </React.StrictMode>
 );
--- src/ronisha.jsx
+++ src/ronisha.jsx
@@ -1,4 +1,19 @@
-function Ronisha() {
-  return <h1>Hi, I'm Ronisha.</h1>;
+import PropTypes from "prop-types";
+
+function Ronisha({ age, favNumber }) {
+  console.log("hello");
+  console.log(age);
+  console.log(favNumber);
+  return (
+    <h1>
+      Hi, I'm Ronisha. My age is {age} and my favorite number is {favNumber}.
+    </h1>
+  );
 }
+
+Ronisha.propTypes = {
+  age: PropTypes.number.isRequired,
+  favNumber: PropTypes.number.isRequired,
+};
+
 export default Ronisha;

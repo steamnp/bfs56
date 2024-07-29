943e492fc57a0c170cf787f727a766018ccd2ee6
lhakpa75 <lhakpa@tumen.nyc>
2024-07-29 11:10:16 -0400

    Parent/Child

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Lhakpa from "./lhakpa";
+import Parent from "./parent";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Lhakpa age={28} favNumber={5} />
+    <Parent />
   </React.StrictMode>
-);
 );
---
+++ src/parent.jsx
@@ -0,0 +1,12 @@
+import Child from "./child";
+
+function Parent() {
+  return (
+    <div>
+      {" "}
+      <Child test="random" />{" "}
+    </div>
+  );
+}
+
+export default Parent;

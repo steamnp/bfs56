221304168a3aebc211a4c80cc630883741aac0fe
adhsandb05 <adhikarisandeeb@gmail.com>
2024-07-29 10:27:01 -0400

    Props in jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Sandeeb from "./sandeeb";
 
-// A component name should be written in PascalCase
-// This is App component
+//console.log is for the debugging purpose
+//{age: 30, favNumber: 7} is the object in javascript, which can be excess by props
+// to access different properties of the object, we can use props.age, props.favNumber
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Sandeeb />
+    <Sandeeb age={30} favNumber={7} />
   </React.StrictMode>
 );
--- src/sandeeb.jsx
+++ src/sandeeb.jsx
@@ -1,5 +1,17 @@
-function Sandeeb() {
-  return <h1>Hi, I am Sandeeb</h1>;
+//DRY -> Don't Repeat Yourself
+//SOC -> Separation of Concerns
+import PropTypes from "prop-types";
+function Sandeeb({ age, favNumber }) {
+  return (
+    <h1>
+      {/* Hi, I am Sandeeb. My age is {props.age} and my favourite number is {props.favNumber}. */}
+      Hi, I am Sandeeb. My age is {age} and my favourite number is {favNumber}.
+    </h1>
+  );
 }
 
+Sandeeb.prototype = {
+  age: PropTypes.number,
+  favNumber: PropTypes.number,
+};
 export default Sandeeb;

94b10aab82ad462c0cdf929f524afbbc73018b30
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-08-01 10:34:49 -0400

    Js variables and functions

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+function displayName(fname, lname) {
+  return fname + " " + lname;
+}
+console.log(displayName("Manish", "Chaudhari"));
+console.log(displayName("Bob", "John"));
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>{displayName("John", "Cena")}</h1>
+    </div>
+  );
 }
 
 export default App;

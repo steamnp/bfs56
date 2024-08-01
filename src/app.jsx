e579a6526dd80825a21aae94c53555ff85fbfb62
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-08-01 19:50:06 +0545

    data types in js

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,42 @@
+// const myName = 'Gorakh' // string
+// console.log(myName)
+// console.log(typeof myName)
+
+// const favNum = 7 // number
+// console.log(favNum)
+// console.log(typeof favNum)
+
+// const numbers = [1, 2, 3, 4, 77] // object -> array
+// console.log(numbers)
+// console.log(typeof numbers)
+
+// const person = {
+//   firstName: 'Gorakh',
+//   middleName: 'Raj',
+//   lastName: 'Joshi',
+// } // object
+// console.log(person)
+// console.log(typeof person)
+
+// let randomNum
+// console.log(randomNum)
+// console.log(typeof randomNum)
+// randomNum = 11
+// console.log(randomNum)
+// console.log(typeof randomNum)
+// randomNum = 12
+// console.log(randomNum)
+// console.log(typeof randomNum)
+
+// const noValue = null
+// console.log(noValue)
+// console.log(typeof noValue)
+
+let test = 10
+test = '20'
+test = [10, 30, 40]
+console.log(test + 5)
+
 function App() {
   return <div>App</div>
 }

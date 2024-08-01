c2e656299d4b6e78c36ed8b4a3a1f516fd4a6521
lhakpa75 <lhakpa@tumen.nyc>
2024-08-01 11:26:26 -0400

    array

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,23 @@
-// const myName = "Lhakpa";
-//console.log(myName);
-//console.log(typeof myName);
+const numbers = [1, 2, 3, 4]; //statement
 
-//const favNum = 7;
-//console.log(favNum);
-//console.log(typeof favNum);
+const imgUrl = [
+  "https://plus.unsplash.com/premium_vector-1722176772341-88058b452488?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxpbGx1c3RyYXRpb25zLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_vector-1722176772341-a6904386c910?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxpbGx1c3RyYXRpb25zLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_vector-1722151898030-5a2d2538d1ed?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxpbGx1c3RyYXRpb25zLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D",
+];
 
-//const numbers = [1, 2, 3, 4, 5];
-//console.log(numbers);
-////console.log(typeof numbers);
+numbers; //expression
 
-//const person = {
-// firstName: "Lhakpa",
-//lastName: "Sherpa",
-//};
-//console.log(person);
-//console.log(typeof person);
-
-//let randomNum;
-//console.log(randomNum);
-//console.log(typeof randomNum);
-
-//randomNum = 11;
-//console.log(randomNum);
-//console.log(typeof randomNum);
-
-//randomNum = 12;
-//console.log(randomNum);
-//console.log(typeof randomNum);
-
-//const noValue = null;
-//console.log(noValue);
-//console.log(typeof noValue);
-
-let test = 10;
-test = "20";
-test = [10, 30, 40];
-console.log(test);
+//console.log(imgUrl);
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      {imgUrl.map(function (imgUrl, index) {
+        return <img key={index} src={imgUrl} />;
+      })}
+    </div>
+  );
 }
 
 export default App;

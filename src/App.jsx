23a8892a7e3118d3d75974278fd4e57ec6094f43
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-08-01 06:41:51 -0700

    different data types and its declaration

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+const myName = "nima"; //string
+console.log(myName);
+
+const favNumber = 7; //Number
+console.log(favNumber);
+
+const numbers = [1, 2, 3, 4, 5]; //Array
+console.log(numbers);
+
+const person = {
+  name: "nima",
+  age: 30,
+}; //Object
+console.log(person);
+
 function App() {
   return <div>App</div>;
 }

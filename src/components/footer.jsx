83ee39d942a9dc3d8260bba29f4410b0ae62c7b7
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-08-01 19:06:37 +0545

    cleanup

--- src/components/footer.jsx
+++
@@ -1,5 +0,0 @@
-function Footer() {
-  return <div className="h-10 bg-amber-900 text-white">Footer</div>
-}
-
-export default Footer
--- src/components/header.jsx
+++
@@ -1,8 +0,0 @@
-// ${} -> Template Literal
-import './header.css'
-
-function Header({ bgColor }) {
-  return <div className={`h-10 text-white test`}>Header</div>
-}
-
-export default Header

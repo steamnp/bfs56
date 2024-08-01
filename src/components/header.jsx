2d19e2dac1474f639e676d4d9369774ed5b6ec94
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-01 09:22:19 -0400

    add app.jsx

--- src/components/header.jsx
+++
@@ -1,5 +0,0 @@
-function Header() {
-  return <div className="h-10 bg-amber-900">Header</div>;
-}
-
-export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.css";
-import Home from "./pages/home.jsx";
+import App from "./app";
 //console.log is for the debugging purpose
 //{age: 30, favNumber: 7} is the object in javascript, which can be excess by props
 // to access different properties of the object, we can use props.age, props.favNumber
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Sandeeb age={30} favNumber={7} /> */}
-    <Home />
+    <App />
   </React.StrictMode>
 );

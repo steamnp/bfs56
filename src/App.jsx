c22a7a8dcb1ddc61e958d7b151d3cb7e3b5b2ccf
lhakpa75 <lhakpa@tumen.nyc>
2024-08-01 10:06:00 -0400

    Day8

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,42 @@
-import React from "react";
+// const myName = "Lhakpa";
+//console.log(myName);
+//console.log(typeof myName);
+
+//const favNum = 7;
+//console.log(favNum);
+//console.log(typeof favNum);
+
+//const numbers = [1, 2, 3, 4, 5];
+//console.log(numbers);
+////console.log(typeof numbers);
+
+//const person = {
+// firstName: "Lhakpa",
+//lastName: "Sherpa",
+//};
+//console.log(person);
+//console.log(typeof person);
+
+//let randomNum;
+//console.log(randomNum);
+//console.log(typeof randomNum);
+
+//randomNum = 11;
+//console.log(randomNum);
+//console.log(typeof randomNum);
+
+//randomNum = 12;
+//console.log(randomNum);
+//console.log(typeof randomNum);
+
+//const noValue = null;
+//console.log(noValue);
+//console.log(typeof noValue);
+
+let test = 10;
+test = "20";
+test = [10, 30, 40];
+console.log(test);
 
 function App() {
   return <div>App</div>;

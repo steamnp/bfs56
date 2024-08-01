0c83e095a2a493dd46fdd9281d358fb58eebeda3
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-01 11:25:59 -0400

    array map

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,21 @@
 //create a function (fuction declaration), use camelCase for function name
 //firstName and lastName are the function parameters
-function displayName(firstName, lastName) {
-  return firstName + lastName;
-}
+const imgUrl = {
+  img1: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fflower%2F&psig=AOvVaw3",
+  img2: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fflower%2F&psig=AOvVaw3",
+  img3: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fflower%2F&psig=AOvVaw3",
+};
 
-// call then function
-//Sandeeb and Adhikari are the function arguments
-console.log(displayName("Sandeeb", "Adhikari"));
+//filter, reduce, ForEach, sort
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      {imgUrl.map(function (imgUrl, index) {
+        return <img src={imgUrl} alt="flower" key={index} />;
+      })}
+    </div>
+  );
 }
 
 export default App;

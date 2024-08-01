4b6cc58d7d31f2a7977fa75b42c763ccffb96a10
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-01 10:05:51 -0400

    console log

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-//console.log is for the debugging purpose
-//{age: 30, favNumber: 7} is the object in javascript, which can be excess by props
-// to access different properties of the object, we can use props.age, props.favNumber
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

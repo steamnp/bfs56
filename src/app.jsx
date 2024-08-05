712c0af3f1dddc1e446e72b515fbca16e806a6e3
Jeneliya12 <gurungjeneliya@gmail.com>
2024-08-05 11:37:40 -0400

    cleanup

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,7 @@
-const numbers = [12, 23, 32, 40]; // statement
+import React from "react";
 
-numbers; //expression
-
-function App() {
-  return (
-    <h1>
-      {numbers.map(function (number) {
-        console.log(number); //will execute the function 4 times
-      })}
-    </h1>
-  );
+function app() {
+  return <div>App</div>;
 }
 
-export default App;
+export default app;

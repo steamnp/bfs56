bde384b323910b4c219721725806e51374332bcc
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-08-05 21:22:31 +0545

    cleanup

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,5 @@
-const person = {
-  firstName: 'Alex',
-  lastName: 'Mohr',
-}
-
 function App() {
-  return (
-    <div>
-      {person.firstName} {person.lastName}
-    </div>
-  )
+  return <div>App</div>
 }
 
 export default App

57f88268f293f8609b5d886f4e4d4b3c3342c5ea
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-05 13:33:23 -0400

    add folder and add file on assets

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,7 @@
-//create a function (fuction declaration), use camelCase for function name
-//firstName and lastName are the function parameters
-const imgUrl = {
-  img1: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fflower%2F&psig=AOvVaw3",
-  img2: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fflower%2F&psig=AOvVaw3",
-  img3: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fflower%2F&psig=AOvVaw3",
-};
+import React from "react";
 
-//filter, reduce, ForEach, sort
-
-function App() {
-  return (
-    <div>
-      {imgUrl.map(function (imgUrl, index) {
-        return <img src={imgUrl} alt="flower" key={index} />;
-      })}
-    </div>
-  );
+function app() {
+  return <div>app</div>;
 }
 
-export default App;
+export default app;

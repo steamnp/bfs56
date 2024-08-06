aecf4ba8808ba672a7ee4d42d7494e80fb8011f1
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-06 11:53:59 -0400

    add assets on public folder

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Home from "./pages/Home";
+import CheckOut from "./components/checkOut";
 
 function app() {
-  return <div>app</div>;
+  return (
+    <div>
+      <Home />
+      <CheckOut />
+    </div>
+  );
 }
 
 export default app;

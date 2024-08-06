8e1ecc6dfacddb29c947106534815a26b4cc3aeb
Amit Gahatraj <amitgahatraj4@gmail.com>
2024-08-06 10:57:18 -0400

    UI design using Figma

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div className="m-6 font-redhat text-[40px] font-bold">
+      <h1>Desserts</h1>
+      <img
+        className="w-[327px] h-[212px]"
+        src="public\assets\images\image-waffle-mobile.jpg"
+      />
+    </div>
+  );
+}
+
+export default Home;

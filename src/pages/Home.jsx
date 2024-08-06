01b43c215e490c687205c1be9585442062489acf
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-06 18:33:25 -0400

    add mobile view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,18 @@ import Waffle from "/assets/images/image-waffle-mobile.jpg";
 function Home() {
   return (
     <div className="m-6">
-      <h1 className="font-redhat font-bold text-[40px]">Desserts</h1>
-      <img src={Waffle} alt="Waffle" className="w-[327px] h-[212px]" />
-      <button className="border border-[AD8A85] rounded-full py-3 px-7 ">
+      <h1 className="font-redhat font-bold text-[40px]">Dessert</h1>
+      <img
+        src={Waffle}
+        alt="Waffle"
+        className="w-[327px] h-[212px] rounded-lg"
+      />
+      <button className="border border-[AD8A85] py-3 px-7 rounded-full">
         Add to Cart
       </button>
+      <div className="m-4">
+        <p>Waffle</p>
+      </div>
     </div>
   );
 }

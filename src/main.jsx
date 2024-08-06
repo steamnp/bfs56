b22fc17c07f0f54ce0e5d47eed9b2a9cf54f3aca
venkatsaikrishnareddy <rayapati001@gannon.edu>
2024-08-06 19:14:50 -0400

    change on krish file

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Krish from "./krish";
 ReactDOM.createRoot(document.getElementById("root")).render(
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Krish from "./krish";
-ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Krish />
   </React.StrictMode>

f27bae6e547462da499af9a07c86dd902f897f53
lhakpa75 <lhakpa@tumen.nyc>
2024-08-06 10:50:34 -0400

    assets

---
+++ src/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="m-6 font-redhat text-[40px] font-bold">
+      <h1>Desserts</h1>
+      <img
+        className="w-[327] h-[212px]"
+        src="assets/images/image-waffle-mobile.jpg"
+        alt="Waffle"
+      />
+      <button className="border border-[#AD8A85] rounded-full w-[160px] h-[44px]" />
+    </div>
+  );
+}
+
+export default Home;

41607eefaf40d93f8bc41ee15bffb55e611300ad
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-08-06 09:04:43 -0700

    product-list-app-firstimage&button

---
+++ src/pages/home.jsx
@@ -0,0 +1,29 @@
+function Home() {
+  return (
+    <div className="m-6">
+      <h1 className="text-[40px] font-bold mb-[32px]">Dessert</h1>
+      <img
+        src="../../assets/images/image-waffle-mobile.jpg"
+        alt="Dessert"
+        className="w-[327px] h-[212px]"
+      />
+      <div className="flex justify-center items-center -mt-6 mb-[16px]">
+        <button className="flex rounded-full  py-3 px-7 border border-[#AD8A85] bg-white hover:text-redText hover:border-redText">
+          <img
+            src="../../assets/images/icon-add-to-cart.svg"
+            alt="Cart"
+            className="mr-2"
+          />
+          Add to Cart
+        </button>
+      </div>
+      <div>
+        <h1 className="text-[#AD8A85] text-2xl pb-[4px]">Waffle</h1>
+        <h1 className="text-2xl pb-[4px]">Waffle with Berries</h1>
+        <h1 className="text-redText  text-2xl">$6.50</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        redText: "#C73B0F",
+      },
+      fontFamily: {
+        sans: ["Red Hat Text", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };

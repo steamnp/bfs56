f5ffc088d2fa724c5b78dae57db1e4b6915c9370
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-07 14:02:10 -0400

    add Red Hat font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import data from "/data.json";
 function Home() {
   return (
     <div className="m-6 w-[375px]">
-      <h1 className="font-redhat font-bold text-[40px]">Dessert</h1>
+      <h1 className="font-bold text-[40px]">Desserts</h1>
       {data.map((item, index) => (
         <Product
           key={index}

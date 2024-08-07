47f7f1a02239d4510084bb0d2f4721c9e6826c3f
lhakpa75 <lhakpa@tumen.nyc>
2024-08-07 11:29:07 -0400

    fixed issue

--- src/component/product.jsx
+++ src/component/product.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useState } from "react";
 
 function Product({ product: { name, category, price } }) {
-  console.log(product);
   const [count, setCount] = useState();
   return (
     <>

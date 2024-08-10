a04ea2bc69edce5a4a9344a9ce8879b77f197bf7
adhsandb05 <adhikarisandeeb@gmail.com>
2024-08-10 14:59:17 -0400

    change component name

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
-import CheckOut from "./components/checkOut";
+import CheckOut from "./components/checkout";
 
 function app() {
   return (

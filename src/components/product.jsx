707ec79b34d849563341e94c8210b12b2490ea6a
Practice <gurungriwaaz7@gmail.com>
2024-08-11 18:19:41 -0400

    map & some fixes

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,37 +1,39 @@
 import { useState } from "react";
 
-function Product({ product: { name, category, price } }) {
+function Product({ item }) {
+  const { image, name, category, price } = item; //destructuring
+  const { thumbnail, mobile, tablet, desktop } = image;
   const [count, setCount] = useState();
   // preValue = 0;
   return (
     <>
-      <div className="w-[327px] h-[234px] mb-4">
-        <img
-          className="rounded-lg"
-          src="assets/images/image-waffle-mobile.jpg"
-          alt="waffle on the table"
-        />
-        <div className="flex justify-center -mt-[22px]">
-          <button
-            onClick={function () {
-              setCount(function (preValue) {
-                return preValue + 1;
-              });
-              console.log(count);
-            }}
-            className="border border-[#AD8A85] rounded-full px-7 py-3 bg-white"
-          >
-            <div className="flex justify-center">
-              <img src="assets/images/icon-add-to-cart.svg" alt="cart icon" />
-              <span> Add to Cart </span>
-            </div>
-          </button>
+      <div className="mt-8 mb-6">
+        <div className="w-[327px] h-[234px] mb-4">
+          <img className="rounded-lg" src={mobile} alt={name} />
+          <div className="flex justify-center -mt-[22px]">
+            <button
+              onClick={function () {
+                setCount(function (preValue) {
+                  return preValue + 1;
+                });
+                console.log(count);
+              }}
+              className="border border-[#AD8A85] rounded-full px-7 py-3 bg-white"
+            >
+              <div className="flex justify-center gap-2">
+                <img src="assets/images/icon-add-to-cart.svg" alt="cart icon" />
+                <span> Add to Cart </span>
+              </div>
+            </button>
+          </div>
         </div>
-      </div>
 
-      <p className="text-[#87635A] text-[14px]">{name}</p>
-      <p className="font-semibold text-[16px]">{category}</p>
-      <p className="text-[#C73B0F] font-semibold text-[16px]">{price}</p>
+        <p className="text-[#87635A] text-[14px]">{category}</p>
+        <p className="font-semibold text-[16px]">{name}</p>
+        <p className="text-[#C73B0F] font-semibold text-[16px]">
+          ${price.toFixed(2)}
+        </p>
+      </div>
     </>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,46 +1,14 @@
 import Product from "../components/product";
 import React from "react";
-//object desctruction
-// const { name } = { name: "John", age: 25 };
-// console.log(name); //John
-//Home component is rendering
-//when sate(count) is updated in react component(Home) will re-render again (run)
+import data from "/data.json";
 function Home() {
-  //array destructuring
-  //const [count, setCount] = useState();
-  //count is a state variable
-  //setCount is a state updating function
-  // const [error, setState] = useState();
-  // const [loading, setLoading] = useState();
-
-  // //display
-  // const [display, setDisplay] = useState();
-
-  // console.log(state); //undefined
-  //console.log(useState()); //useState is a function
-  //console.log(count);
   return (
     <div className="m-6 w-[375px] ">
       <h1 className="font-bold mb-[32px] text -[40px]">Dessert</h1>
-      <Product
-        product={{
-          name: "Waffle",
-          category: "Waffle with Berries",
-          price: "$ 6.50",
-        }}
-      />
-      {/* <Product product={{ name: "A", category: "catA", price: 12 }} />
-      <Product product={{ name: "B", category: "catB", price: 12 }} /> */}
+      {data.map((item, index) => (
+        <Product key={index} item={item} />
+      ))}
     </div>
   );
 }
 export default Home;
-
-//     <div className=" m-6 font-redhat w-[327px] h-[69px]">
-//       <p className="h-[19px] w-[41px] text-sm text-[#87635A] ">Waffle</p>
-//       <h1 className="w-[142px] h-[21px] font-semibold text-[#260F08]">
-//         Waffle with Berries
-//       </h1>
-//       <h1 className=" text-[#C73B0F] ">6.50</h1>
-
-//

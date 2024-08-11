f2704d6618e11fbe452ce2f4c6cc5fcc43a0710c
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-08-11 13:24:30 -0400

    added hover effect to Add to cart button

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,28 +1,38 @@
 import { useState } from "react";
+import data from "/Users/manish/BF556FRONTED/viteapp1/data.json";
 
 function Product({ item }) {
   const [state, setState] = useState();
   const { image, name, category, price } = item;
-  const { mobile } = image;
+  const { desktop, mobile, tablet, thumbnail } = image;
 
+  const cleanImageUrl = (image) => {
+    return image.replace("./", "./");
+  };
   return (
-    <div className="">
-      <img
-        className="object-fill w-[327px] h-[212px]"
-        src={mobile}
-        alt="square waffles on a plate with strawberries as toppings "
-      ></img>
-      <div className="flex justify-center my-[-22px]">
-        <button className="border flex items-center  py-3 px-7  border-[#AD8A85] rounded-full">
+    <div className="card mb-0">
+      <picture>
+        <source media="(width < 640px)" srcSet={cleanImageUrl(mobile)} />
+        <source media="(width < 768px)" srcSet={cleanImageUrl(tablet)} />
+        <img
+          className="rounded-2xl "
+          src={cleanImageUrl(desktop)}
+          alt={`Image ${name}`}
+        />
+      </picture>
+      <div className="flex justify-center ">
+        <button className="flex items-center  py-[12.5px] px-7 mt-[-22px] space-x-2 border-2 bg-white border-[#AD8A85] hover:border-rose-900 rounded-full">
           <img
             className="w-[20px] h-[20px]"
             src="/assets/images/icon-add-to-cart.svg"
             alt="Add to cart icon"
           ></img>
-          <span className="m-2">Add to Cart</span>
+          <span className="font-semibold hover:text-rose-900 px-1">
+            Add to Cart
+          </span>
         </button>
       </div>
-      <div className="m-4">
+      <div className="card-description">
         <h1>{category}</h1>
         <h1>{name}</h1>
         <h1>{price}</h1>

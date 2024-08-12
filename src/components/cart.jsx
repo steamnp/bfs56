09e3733fbd1b206d8464db8dfb441761ee68fa80
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-08-12 09:55:48 -0400

    updated cart styling

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function Cart() {
   let count = 1;
   return (
-    <div className="card mb-4 min-h-4">
+    <div className="bg-white p-8 h-fit rounded-lg">
       <div>
-        <h1 className="text-[24px] text-red-600 font-black">
+        <h2 className="text-primary text-red-600 font-black">
           Your Cart ({count})
-        </h1>
+        </h2>
       </div>
       <svg>
         <image
-          className=" py-[80px]"
+          className="py-[80px]"
           href="/assets/images/illustration-empty-cart.svg"
         ></image>
       </svg>

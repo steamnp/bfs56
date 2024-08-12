ff9057e23ec40088f046bc6bcf5efa1b187b7839
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-08-12 10:37:42 -0400

    initialized app using typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        tomato: "#ff6347",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };

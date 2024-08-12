f9d4ef020f656c055f2245cf1c2e0d82a2e5c59a
Jeneliya12 <gurungjeneliya@gmail.com>
2024-08-12 10:41:18 -0400

    migrate javascript to typescript

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="m-6 flex flex-col lg:flex-row lg:gap-6">
+      <div className="flex-1">
+        <h1 className="font-redhat text-[30px] mb-6 bg-red-500 w-[100px]">
+          Desserts
+        </h1>
+      </div>
+      <div className="lg:w-[327px]"></div>
+    </div>
+  );
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Ensure all file types and paths are covered
-    "./public/index.html", // Include paths to HTML files
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    colors: {
-      red: "#C73B0F",
-      "rose-900": "#260F08",
-      "rose-500": "#87635A",
-      "rose-400": "#AD8A85",
-      "rose-50": "#FCF8F6",
-      "grey-100": "#f3f4f6",
-      white: "#ffffff",
-    },
   },
   plugins: [],
 };

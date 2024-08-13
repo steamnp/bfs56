71e19084d96a24ddb18cb2b106d8699f34bd2221
Amit Gahatraj <amitgahatraj4@gmail.com>
2024-08-13 08:41:05 -0400

    populate data in frontend using map

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,23 +1,24 @@
-function Product() {
+function Product({ name, category, price, image }) {
   return (
     <div>
-      <img
-        className="w-[327px] h-[212px] rounded-lg"
-        src="public\assets\images\image-waffle-mobile.jpg"
-      />
+      <img className="w-[327px] h-[212px] rounded-lg " src={image.mobile} />
       <div className="flex justify-center w-[327px] mt-[-22px]">
         <button className="flex items-center rounded-full  py-3 px-7 bg-white border border-[#AD8A85]">
-          <img src="public\assets\images\cart_image.png" alt="Cart image" />
+          <img
+            className="size-5"
+            src="public\assets\images\cart_image.png"
+            alt="Cart image"
+          />
           <h1 className="text-[14px]">Add to Cart</h1>
         </button>
       </div>
       <div>
-        <h1 className="text-[14px] text-[#87635A] font-redhat">Waffle</h1>
+        <h1 className="text-[14px] text-[#87635A] font-redhat">{category}</h1>
         <h1 className="text-[16px] font-redhat text-[#260F08] font-semibold">
-          Waffle with Berries
+          {name}
         </h1>
         <h1 className="font-redhat text-[16px] text-[#C73B0F] font-semibold">
-          $6.50
+          ${price}
         </h1>
       </div>
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,20 @@
 import Product from "../components/product";
+import data from "../../data.json";
 
 function Home() {
   return (
     <div>
       <div className="m-6 w-[375px] ">
         <h1 className="font-redhat text-[40px] font-bold">Desserts</h1>
-        <Product />
+        {data.map((item, index) => (
+          <Product
+            key={index}
+            name={item.name}
+            category={item.category}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
       </div>
     </div>
   );

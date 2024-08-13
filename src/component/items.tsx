39f1138a3d3250dcb49be88f0944ec667b2113ef
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-08-13 06:33:02 -0700

    fix the resolution

--- src/component/items.tsx
+++ src/component/items.tsx
@@ -5,8 +5,8 @@ function Items({ product }) {
     <div className="flex flex-col mb-6 ">
       <div className="relative">
         <picture>
-          <source media="(min-width: 768px)" srcSet={desktop} />
-          <source media="(min-width: 640px)" srcSet={tablet} />
+          <source media="(min-width: 1440px)" srcSet={desktop} />
+          <source media="(min-width: 768px)" srcSet={tablet} />
           <img className="rounded-lg w-full" src={mobile} alt={name} />
         </picture>
         <button className="absolute bottom-0 justify-center items-center w-[160px] left-1/2 transform -translate-x-1/2 translate-y-1/2 flex py-3 px-6 border-2 rounded-full bg-white shadow-md hover:border-buttonBorderColor hover:text-redText">
@@ -15,7 +15,7 @@ function Items({ product }) {
             className=""
             alt="cart"
           />
-          <span className="font-semibold text-sm">Add to Cart</span>
+          <span className="font-semibold text-sm pl-2">Add to Cart</span>
         </button>
       </div>
       <div className="mt-10">

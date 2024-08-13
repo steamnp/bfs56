4893143fc70aa433631fec165e93277ec80214b7
Amit Gahatraj <amitgahatraj4@gmail.com>
2024-08-13 04:19:00 -0400

    switch from JS to TS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from "./pages/home";
+
+function App() {
+  //const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <Home />
+      </div>
+    </>
+  );
+}
+
+export default App;

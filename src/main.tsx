00d5903610489ea3cdeabdef61a21af7f7932549
Jeneliya12 <gurungjeneliya@gmail.com>
2024-08-13 09:51:42 -0400

    cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/home";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>

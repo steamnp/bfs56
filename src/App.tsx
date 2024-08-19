8367297f90ef31238932817aea6920ed856c5ee1
Sandeeb Adhikari <sandeebadhikari@sandeebs-mbp.mynetworksettings.com>
2024-08-19 11:06:38 -0400

    explored promise

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+
+const apiURL = "https://restcountries.com/v3.1/all";
+
 function app() {
-  return <>App</>;
+  useEffect(() => {
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((anotherRes) => console.log(anotherRes))
+      .catch((error) => console.log(error.message));
+  });
+
+  return <>Loading....</>;
 }
 
 export default app;

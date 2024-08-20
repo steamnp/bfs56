a5d3b6f33397f6332e87a9a4939627658e79766b
Ronisha Shigdel <ronishashigdel@gmail.com>
2024-08-19 22:16:06 -0700

    add custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import useFetch from "./useFetch";
+const apiUrl = "https://restcountries.com/v3.1/all";
 
 function App() {
-  return <div>App</div>;
-}
+  const { countries, loading } = useFetch(apiURL);
+
+  return loading ? (
+    <div>Loading....</div>
+  ) : (
+    <div>
+      {countries.map((country, index) => (
+        <div key={index}>
+          <h1>{country.name.common}</h1>
+          <img src={country.flags.png} alt={country.name.common} />
+        </div>
+      ))}
+    </div>
+  );
 
 export default App;

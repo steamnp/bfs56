08e8ebabeb611803627e50ad9cf257f77f39b2be
Sandeeb Adhikari <sa2734@njit.edu>
2024-08-20 09:40:25 -0400

    add till Updating contacts with formData

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,33 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-
+import Contact, { loader as contactLoader } from "./routes/contact";
 import "./index.css";
+import EditContact, { action as editAction } from "./routes/edit";
+
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },

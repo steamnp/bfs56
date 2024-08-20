3857c5a5d4443fc1a7649bf4c9fef9af9d7c6180
Practice <gurungriwaaz7@gmail.com>
2024-08-20 11:50:39 -0400

    reduxpart

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import Home from "./pages/Home";
-import Cart from "./components/cart";
-import { cartContext } from "./context/product";
-import { useState } from "react";
+import React from "react";
 
-function App() {
-  const [cartItems, setCartItems] = useState();
-  // const cartvalue = {
-  //   cart,
-  //   setCart,
-  // };
-  //useContext(cartContext);
-  return (
-    <>
-      <main className="container mx-auto px-4 py-16">
-        <div className="grid gap-8 grid-cols-1 sm:grid-cols-[1fr_33%]">
-          <Home />
-          <cartContext.Provider value={[cartItems, setCartItems]}>
-            <Cart />
-          </cartContext.Provider>
-        </div>
-      </main>
-    </>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;

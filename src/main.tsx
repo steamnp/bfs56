e93a10af288b23e0c58ae20ddedd4e08bb83fef5
Sandeeb Adhikari <sa2734@njit.edu>
2024-08-20 10:12:54 -0400

    organising

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
+
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import "./index.css";

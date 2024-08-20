b9b7666395259c17102fab5031c6d79adc6528a7
Jeneliya12 <gurungjeneliya@gmail.com>
2024-08-19 21:01:19 -0400

    fetch api successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import React, { useEffect } from "react";
-const apiUrl = "https://restcountries.com/v3.1/all";
+import React, { useEffect, useState } from "react";
+
+// Make sure to use the correct API URL
+const apiUrl = "https://restcountries.com/v3.1/name/nepal";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    //synchronous
     fetch(apiUrl)
       .then((res) => res.json())
-      .then((anotherRes) => console.log(anotherRes));
-  });
-  return <div>Loading...</div>;
+      .then((data) => {
+        setCountries(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      {countries.map((country, index) => (
+        <div key={index}>
+          <h1>{country.name.official}</h1>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;

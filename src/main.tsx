6ed04b1280bddeb5842813022d6d8edfd74d063d
Sandeeb Adhikari <sa2734@njit.edu>
2024-08-21 09:30:40 -0400

    initilize redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <div>Hello</div>
+    </Provider>
+  </React.StrictMode>
+);
---
+++ src/pages/useSelector.ts
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+
+function Home() {
+  const products = useSelector((state) => {
+    return state.product;
+  });
+
+  return (
+    <ul>
+      {products.map((e) => {
+        return <li key={e.id}>{e.name}</li>;
+      })}
+    </ul>
+  );
+}
+
+export default Home;

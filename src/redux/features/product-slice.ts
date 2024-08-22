948b510b4eab718f659be05f42c6f22c5572c37b
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-08-22 18:48:07 +0545

    create slice

---
+++ src/redux/features/product-slice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const productSlice = createSlice({
+  name: 'product',
+  initialState: 'Random Product',
+  reducers: {},
+})
+
+export default productSlice.reducer
---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+import productReducer from './features/product-slice'
+import userSlice from './features/user-slice'
+import counterSlice from './features/counter'
+
+export const store = configureStore({
+  reducer: {
+    product: productReducer,
+    user: userSlice,
+    counter: counterSlice,
+  },
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch

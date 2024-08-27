67154fd1a38bd220fd5e1fb8e985b7032727d7b4
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-08-27 07:26:03 -0700

    fullstack

---
+++ ecommerce-backend/controllers/blogController.js
@@ -0,0 +1,185 @@
+import Blog from "../models/blogModel.js";
+import asyncHandler from "express-async-handler";
+import { cloudinaryUploadImg } from "../utils/cloudinary.js";
+import fs from "fs";
+
+import { validateMongoID } from "../utils/validateMongoId.js";
+
+export const createBlog = asyncHandler(async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+    res.json(newBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const updateBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoID(id);
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const getBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoID(id);
+  try {
+    const getaBlog = await Blog.findById(id)
+      .populate("likes")
+      .populate("dislikes");
+    await Blog.findByIdAndUpdate(id, { $inc: { numViews: 1 } }, { new: true });
+    res.json(getaBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const getAllBlogs = asyncHandler(async (req, res) => {
+  try {
+    const allBlogs = await Blog.find();
+    res.json(allBlogs);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const deleteBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoID(id);
+  try {
+    const deletedBlog = await Blog.findByIdAndDelete(id);
+    res.json(deletedBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const liketheBlog = asyncHandler(async (req, res) => {
+  const { blogId } = req.body;
+  validateMongoID(blogId);
+  // Find the blog which you want to be liked
+  const blog = await Blog.findById(blogId);
+  // find the login user
+  const loginUserId = req?.user?._id;
+  // find if the user has liked the blog
+  const isLiked = blog?.isLiked;
+  // find if the user has disliked the blog
+  const alreadyDisliked = blog?.dislikes?.find(
+    (userId) => userId?.toString() === loginUserId?.toString()
+  );
+  if (alreadyDisliked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { dislikes: loginUserId },
+        isDisliked: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+  if (isLiked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: loginUserId },
+        isLiked: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  } else {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { likes: loginUserId },
+        isLiked: true,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+});
+
+export const disliketheBlog = asyncHandler(async (req, res) => {
+  const { blogId } = req.body;
+  // Find the blog which you want to be liked
+  const blog = await Blog.findById(blogId);
+  // find the login user
+  const loginUserId = req?.user?._id;
+  // find if the user has liked the blog
+  const isDisLiked = blog?.isDisLiked;
+  // find if the user has disliked the blog
+  const alreadyLiked = blog?.likes?.find(
+    (userId) => userId?.toString() === loginUserId?.toString()
+  );
+  if (alreadyLiked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: loginUserId },
+        isLiked: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+  if (isDisLiked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { dislikes: loginUserId },
+        isDisLiked: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  } else {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { dislikes: loginUserId },
+        isDisliked: true,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+});
+
+export const uploadImages = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoID(id);
+  try {
+    const uploader = (path) => cloudinaryUploadImg(path, "images");
+    const urls = [];
+    const files = req.files;
+    for (const file of files) {
+      const { path } = file;
+      const newpath = await uploader(path);
+      urls.push(newpath);
+      // fs.unlinkSync(path);
+    }
+    const findBlog = await Blog.findByIdAndUpdate(
+      id,
+      {
+        images: urls.map((file) => {
+          return file;
+        }),
+      },
+      {
+        new: true,
+      }
+    );
+    res.json(findBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
---
+++ ecommerce-front-end/src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from "../features/auth/authSlice"
+import productReducer from "../features/products/productsSlice"
+import blogReducer from "../features/blog/blogSlice"
+
+
+export const store = configureStore({
+  reducer: {
+    auth:authReducer,
+    products:productReducer,
+    blogs:blogReducer
+  },
+  devTools:false
+});

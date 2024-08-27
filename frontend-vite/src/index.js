62ed8a9436329c0d3f05ea93462afd7a07e7820a
Ronisha Shigdel <ronishashigdel@gmail.com>
2024-08-27 08:42:15 -0700

    migrate to vite

---
+++ frontend-vite/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { store } from "./app/store";
+import App from "./App";
+import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.meta.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);

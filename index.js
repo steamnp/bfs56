b6bf9f18b4dc469a9b8adffde78cf4c116cd785f
Sandeeb Adhikari <sa2734@njit.edu>
2024-08-28 10:46:19 -0400

    middleware to get data and error

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-import express from 'express'
+import express from "express";
+import { errorHandler, notFound } from "./middleware/error";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
 // listen()
 // get()
 
 app.listen(4000, () => {
-  console.log('I am running in port 4000')
-})
+  console.log("I am running in port 4000");
+});
 
 // http://localhost:4000/api/product
 // REST API
-app.get('/api/product', (req, res, next) => {
-  const output = { name: 'iPhone' }
-  res.json(output)
-})
+app.get("/api/product", (req, res, next) => {
+  const output = { name: "iPhone" };
+  res.json(output);
+});
 
-app.get('/api/product/new', (req, res, next) => {
-  const output = { name: 'new response' }
-  res.json(output)
-})
+app.get("/api/product/new", (req, res, next) => {
+  const output = { name: "new response" };
+  res.json(output);
+});
+
+app.use(notFound);
+app.use(errorHandler);

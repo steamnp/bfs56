cbfc4231f11782aaa7d35be3b19a6093c4b16285
Sandeeb Adhikari <sa2734@njit.edu>
2024-08-28 18:34:29 -0400

    bootstrap added

--- frontend_vite/src/index.jsx
+++ frontend_vite/src/index.jsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 if (process.env.NODE_ENV === "production") {
   disableReactDevTools();

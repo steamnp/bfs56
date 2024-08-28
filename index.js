fbf4e91923b4f17e1f6dd890fb241bba2d64b937
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-08-28 10:54:31 -0700

    crud

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-import express from 'express'
+import express from "express";
+import { errorHandler, notFound } from "./middleware/error.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
+
+const app = express();
 
 // listen()
 // get()
+//vLxDBlJAhwMsqJlX
 
 app.listen(4000, () => {
-  console.log('I am running in port 4000')
-})
+  console.log("I am running in port 4000");
+});
 
 // http://localhost:4000/api/product
 // REST API
-app.get('/api/product', (req, res, next) => {
-  const output = { name: 'iPhone' }
-  res.json(output)
-})
+app.get("/api/product", (req, res, next) => {
+  const output = { name: "iPhone" };
+  res.json(output);
+});
 
-app.get('/api/product/new', (req, res, next) => {
-  const output = { name: 'new response' }
-  res.json(output)
-})
+app.get("/api/product/new", (req, res, next) => {
+  const output = { name: "new response" };
+  res.json(output);
+});
+
+app.get("/", (req, res, next) => {
+  res.json("hello");
+});
+
+//post request
+
+app.post("/register", (req, res, next) => {
+  console.log("Post is called");
+  res.json("this is post");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+mongoose
+  .connect(process.env.mongo_url)
+  .then(() => {
+    console.log("connected to database!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ middleware/error.js
@@ -0,0 +1,15 @@
+//function for 1st middleWare --> sends to next error function
+export const notFound = (req, res, next) => {
+  const error = new Error(`Not Found: ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+export const errorHandler = (err, req, res, next) => {
+  const statuscode = res.statusCode == 200 ? 500 : res.statusCode;
+  res.status(statuscode);
+  res.json({
+    message: err?.message,
+    stack: err?.stack,
+  });
+};

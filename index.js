bc10b95ed7860dce95037ba249b3b0eebdbfc3bc
Ronisha Shigdel <ronishashigdel@gmail.com>
2024-08-29 07:57:05 -0700

    basic CRUD operations

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello backend");
+import express from "express";
+import { errorHandler, notFound } from "./middleware/error.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { userRouter } from "./routes/user.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json()); // parse incoming data
+app.listen(4000, () => {
+  console.log("I am running in port 4000");
+});
+
+app.use("/api/user", userRouter);
+app.use(notFound);
+app.use(errorHandler);
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to Database!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });

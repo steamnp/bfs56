2cbcb9128d342aeb225ae8fe9675e24795c1204f
Ronisha Shigdel <ronishashigdel@gmail.com>
2024-08-28 22:47:01 -0700

    delete old frontend

--- frontend/src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
-
-const Loader = ({ type, color }) => (
-	<ReactLoading type={type} color={color} height={'1%'} width={'1%'} />
-);
-
-export default Loader;
